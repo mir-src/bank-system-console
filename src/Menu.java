422ac650b45ae69d7057b4e3b1391d68a62c9fb4
mir-src <264200162+mir-src@users.noreply.github.com>
2026-08-28 20:31:12 +0300

    Implemented: Display Accounts

--- src/Menu.java
+++ src/Menu.java
@@ -16,6 +16,7 @@ public class Menu {
             System.out.println("4. Delete Account");
             System.out.println("5. Enter another account");
             System.out.println("6. Exit App");
+            System.out.println("7. Display Accounts");
 
             String choice = IO.readln();
             switch(choice){
@@ -61,8 +62,14 @@ public class Menu {
                         System.out.println("One of the accounts doesn't exist");
                     }
                     break;
-                case "7":
+                case "6":
                     return;
+                case "7":
+                    System.out.println("ACCOUNTS...");
+                    for (Account item : accounts){
+                        System.out.println("Name: " + item.getName() + " | Money: " + item.getAmount());
+                    }
+                    break;
             }
             System.out.print("\033[H\033[2J");
             System.out.flush();
